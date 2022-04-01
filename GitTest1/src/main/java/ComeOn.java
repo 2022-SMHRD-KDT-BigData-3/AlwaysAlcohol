9af6ab970a0873d861f5a4ec20e04adf4b9f60ec
smhrd <smhrd@DESKTOP-S5NAS2Q>
2022-04-01 12:19:50 +0900

    커밋

--- GitTest1/src/main/java/ComeOn.java
+++ GitTest1/src/main/java/ComeOn.java
@@ -11,7 +11,7 @@ public class ComeOn {
 		// 중요! pull,push 하기 전에는 꼭
 		// Local repository를 최신화 시켜줘야한다(이클립스 코드와 같아야한다.) == 커밋을 해줘야 한디.
 		
-		System.out.println("다섯번째 커밋");
+		System.out.println("기택 커밋");
 	}
 	
 	
