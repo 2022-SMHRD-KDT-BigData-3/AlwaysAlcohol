51c61915c2c144be603d0e2836ca4ad2a8c4549f
smhrd <smhrd@DESKTOP-OVUO4P0>
2022-04-01 12:26:34 +0900

    준식커밋

--- GitTest1/src/main/java/ComeOn.java
+++ GitTest1/src/main/java/ComeOn.java
@@ -5,21 +5,13 @@ public class ComeOn {
 		
 		System.out.println("첫 번째 커밋");
 		System.out.println("두 번째 커밋");
-<<<<<<< HEAD
-		// pull push --> git 원격 저장소와
-		// Local repository 의 통신
-		// 중요 ! pull push를 하기 전에는 꼭
-		// Local repository를 최신화 시켜줘야한다!(이클립스 코드와 같아야한다)
-		// == 커밋을 해줘야 한다.
-=======
-		
+
 		// pull, push --> git원격 저장소
 		// local repository의 통신
 		// 중요! pull,push 하기 전에는 꼭
 		// Local repository를 최신화 시켜줘야한다(이클립스 코드와 같아야한다.) == 커밋을 해줘야 한디.
 		
-		System.out.println("기택 커밋");
->>>>>>> branch 'master' of https://github.com/2022-SMHRD-KDT-BigData-3/AlwaysAlcohol.git
+
 	}
 	
 	
