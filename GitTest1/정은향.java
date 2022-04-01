1b3b8a764c3eef982892e937d9c459e80dc3c8fc
정은향 <101630907+jungeunhyang@users.noreply.github.com>
2022-04-01 12:26:41 +0900

    Add files via upload

---
+++ GitTest1/정은향.java
@@ -0,0 +1,16 @@
+
+public class 정은향 {
+
+	public static void main(String[] args) {
+		// pull, push --> git원격 저장소와
+		// Local repository 의 통신이다.
+
+		// 중요! pull, push 하기 전에는 꼭 !!
+		// Local repository를 최신화 시켜줘야한다!!(이클립스 코드와 같아야한다)
+		// == commit을 해줘야 한다!!!
+		
+		System.out.println("commit");
+
+	}
+
+}
