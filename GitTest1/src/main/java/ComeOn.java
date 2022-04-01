5ff14f22c3ee67c32b4dd8548cecd6939637e16e
smhrd <smhrd@DESKTOP-PEE72S5>
2022-04-01 12:21:23 +0900

    Ok?

--- GitTest1/src/main/java/ComeOn.java
+++ GitTest1/src/main/java/ComeOn.java
@@ -5,6 +5,13 @@ public class ComeOn {
 		
 		System.out.println("첫 번째 커밋");
 		System.out.println("두 번째 커밋");
+		// pull, push -> git원격 저장소와
+		// Local repository
+		// !! pull, push하기 전에는 꼭!!
+		// !! Local repository를 최신화 시켜줘야 한다
+		// 커밋을 해줘야 한다!!!!!!
+		
+		System.out.println("다섯 번째 커밋");
 	}
 	
 	
