5ee7cef0fff6f13dd8c68d9da998ec79392cb943
smhrd <smhrd@DESKTOP-HJUBL30>
2022-04-02 13:54:40 +0900

    DB 보강 필요

--- GitTest1/src/main/webapp/project.sql
+++ GitTest1/src/main/webapp/project.sql
@@ -3,32 +3,54 @@
 --프로젝트DB의 테이블은 전부 삭제했습니다
 
 DROP TABLE member CASCADE CONSTRAINTS;
+DROP TABLE base CASCADE CONSTRAINTS;
+DROP TABLE taste CASCADE CONSTRAINTS;
 DROP TABLE cocktail CASCADE CONSTRAINTS;
 DROP TABLE recipe CASCADE CONSTRAINTS;
-DROP TABLE fav_list CASCADE CONSTRAINTS;
+DROP TABLE drink_list CASCADE CONSTRAINTS;
 DROP TABLE reply CASCADE CONSTRAINTS;
+DROP TABLE fav_list CASCADE CONSTRAINTS;
 
 --사용자
 --선호 관련 구문 추가 필요
+-- 남자 : 0, 여자 : 1
 CREATE TABLE member
 (member_id VARCHAR(15),
  pw VARCHAR(20) NOT NULL,
  nick VARCHAR(25) NOT NULL,
  member_name VARCHAR(25) NOT NULL,
- gender VARCHAR(15),
+ gender VARCHAR(2),
  birth DATE NOT NULL,
  join_date DATE DEFAULT SYSDATE NOT NULL,
  CONSTRAINT mem_id_pk PRIMARY KEY(member_id),
- CONSTRAINT mem_gender_ck CHECK(gender IN('MALE','FEMALE')));
+ CONSTRAINT mem_gender_ck CHECK(gender IN('0','1')));
  
- --칵테일
+--기주(베이스)
+CREATE TABLE base
+(base_id NUMBER(10),
+ base_name VARCHAR(15),
+ CONSTRAINT base_id_pk PRIMARY KEY(base_id));
+ 
+--맛
+CREATE TABLE taste
+(taste_id NUMBER(10),
+ taste_info VARCHAR(20) NOT NULL,
+ taste_info2 VARCHAR(20),
+ taste_info3 VARCHAR(20),
+ CONSTRAINT taste_id_pk PRIMARY KEY(taste_id));
+ 
+--칵테일
 CREATE TABLE cocktail
 (cocktail_id NUMBER(10),
  cocktail_name VARCHAR(15) NOT NULL,
- base VARCHAR(15) NOT NULL,
+ cocktail_info VARCHAR(100) NOT NULL,
  proof NUMBER(5) NOT NULL,
- taste VARCHAR(15) NOT NULL,
- CONSTRAINT cocktail_id_pk PRIMARY KEY(cocktail_id));
+ base_id NUMBER(10),
+ taste_id NUMBER(10),
+ cocktail_img VARCHAR(200) NOT NULL,
+ CONSTRAINT cocktail_id_pk PRIMARY KEY(cocktail_id),
+ CONSTRAINT cocktail_base_fk FOREIGN KEY(base_id) REFERENCES base(base_id),
+ CONSTRAINT cocktail_taste_fk FOREIGN KEY(taste_id) REFERENCES taste(taste_id));
  
  --칵테일 레시피
 CREATE TABLE recipe
@@ -39,15 +61,15 @@ CREATE TABLE recipe
  CONSTRAINT recipe_id_pk PRIMARY KEY(recipe_id),
  CONSTRAINT recipe_cock_fk FOREIGN KEY(cocktail_id) REFERENCES cocktail(cocktail_id));
  
- --사용자의 선호 목록
-CREATE TABLE fav_list
-(fav_id NUMBER(10),
+ --사용자의 칵테일 찜목록
+CREATE TABLE drink_list
+(dlist_id NUMBER(10),
  member_id VARCHAR(15),
  cocktail_id NUMBER(10),
- fav_join DATE DEFAULT SYSDATE NOT NULL,
- CONSTRAINT fav_id_pk PRIMARY KEY(fav_id),
- CONSTRAINT fav_mem_fk FOREIGN KEY(member_id) REFERENCES member(member_id),
- CONSTRAINT fav_cock_fk FOREIGN KEY(cocktail_id) REFERENCES cocktail(cocktail_id));
+ dlist_join DATE DEFAULT SYSDATE NOT NULL,
+ CONSTRAINT dlist_id_pk PRIMARY KEY(dlist_id),
+ CONSTRAINT dlist_mem_fk FOREIGN KEY(member_id) REFERENCES member(member_id),
+ CONSTRAINT dlist_cock_fk FOREIGN KEY(cocktail_id) REFERENCES cocktail(cocktail_id));
  
  --칵테일의 댓글(평가 포함)
 CREATE TABLE reply
@@ -55,21 +77,36 @@ CREATE TABLE reply
  member_id VARCHAR(15),
  cocktail_id NUMBER(10),
  reply_score NUMBER(10) NOT NULL,
- reply_content VARCHAR(20) NOT NULL,
+ reply_content VARCHAR(50) NOT NULL,
  reply_date DATE DEFAULT SYSDATE NOT NULL,
  CONSTRAINT reply_id_pk PRIMARY KEY(reply_id),
  CONSTRAINT reply_mem_fk FOREIGN KEY(member_id) REFERENCES member(member_id),
  CONSTRAINT reply_cock_fk FOREIGN KEY(cocktail_id) REFERENCES cocktail(cocktail_id));
  
+CREATE TABLE fav_list
+(flist_id NUMBER(30),
+ member_id VARCHAR(15),
+ base_id NUMBER(10),
+ taste_id NUMBER(10),
+ cocktail_id NUMBER(10),
+ reply_id NUMBER(10),
+ CONSTRAINT flist_id_pk PRIMARY KEY(flist_id),
+ CONSTRAINT flist_mem_fk FOREIGN KEY(member_id) REFERENCES member(member_id),
+ CONSTRAINT flist_base_fk FOREIGN KEY(base_id) REFERENCES base(base_id),
+ CONSTRAINT flist_taste_fk FOREIGN KEY(taste_id) REFERENCES taste(taste_id),
+ CONSTRAINT flist_cock_fk FOREIGN KEY(cocktail_id) REFERENCES cocktail(cocktail_id),
+ CONSTRAINT flist_reply_fk FOREIGN KEY(reply_id) REFERENCES reply(reply_id));
+ 
 SELECT * FROM member;
+SELECT * FROM base;
+SELECT * FROM taste;
 SELECT * FROM cocktail;
 SELECT * FROM recipe;
-SELECT * FROM fav_list;
+SELECT * FROM drink_list;
 SELECT * FROM reply;
+SELECT * FROM fav_list;
 
-INSERT INTO member VALUES();
-
-
+INSERT INTO member VALUES('test','test','test','test','0','2022-04-02',SYSDATE);
 
 
 
