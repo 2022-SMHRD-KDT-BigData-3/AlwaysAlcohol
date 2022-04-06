9ce9103c175ac8f3c34b658396dc5fde0801c1ac
smhrd <smhrd@DESKTOP-HJUBL30>
2022-04-06 16:07:33 +0900

    와인관련 테이블 임포트 완료

--- GitTest1/src/main/webapp/project.sql
+++ GitTest1/src/main/webapp/project.sql
@@ -28,53 +28,53 @@ CREATE TABLE member
 --산도
 CREATE TABLE acid
 (acid_id NUMBER(10),
- acid_name VARCHAR2(20) NOT NULL,
+ acid_name VARCHAR2(50) NOT NULL,
  CONSTRAINT acid_id_pk PRIMARY KEY(acid_id));
  
 --바디감
 CREATE TABLE body
 (body_id NUMBER(10),
- body_name VARCHAR2(30) NOT NULL,
+ body_name VARCHAR2(50) NOT NULL,
  CONSTRAINT body_id_pk PRIMARY KEY(body_id));
  
 --당도
 CREATE TABLE sweet
 (sweet_id NUMBER(10),
- sweet_name VARCHAR2(10) NOT NULL,
+ sweet_name VARCHAR2(50) NOT NULL,
  CONSTRAINT sweet_id_pk PRIMARY KEY(sweet_id));
  
 --국가
 CREATE TABLE country
 (country_id NUMBER(10),
- county_name VARCHAR2(10) NOT NULL,
+ county_name VARCHAR2(50) NOT NULL,
  CONSTRAINT country_id_pk PRIMARY KEY(country_id));
  
 --종류
 CREATE TABLE kind
 (kind_id NUMBER(10),
- kind_name VARCHAR2(10) NOT NULL,
+ kind_name VARCHAR2(50) NOT NULL,
  CONSTRAINT kind_id_pk PRIMARY KEY(kind_id));
  
 --타닌
 CREATE TABLE tanin
 (tanin_id NUMBER(10),
- tanin_name VARCHAR(20) NOT NULL,
+ tanin_name VARCHAR(50) NOT NULL,
  CONSTRAINT tanin_id_pk PRIMARY KEY(tanin_id));
  
 --와인
 CREATE TABLE wine
 (wine_id NUMBER(10),
- wine_name VARCHAR2(10) NOT NULL,
+ wine_name VARCHAR2(300) NOT NULL,
  kind_id NUMBER(10),
  country_id NUMBER(10),
- cultivar VARCHAR2(50),
+ cultivar VARCHAR2(300),
  sweet_id NUMBER(10),
  acid_id NUMBER(10),
  body_id NUMBER(10),
  tanin_id NUMBER(10),
- proof VARCHAR2(20),
- price VARCHAR2(20),
- img VARCHAR2(200) NOT NULL,
+ proof VARCHAR2(300),
+ price VARCHAR2(300),
+ img VARCHAR2(999) NOT NULL,
  CONSTRAINT wine_id_pk PRIMARY KEY(wine_id),
  CONSTRAINT wine_kind_fk FOREIGN KEY(kind_id) REFERENCES kind(kind_id),
  CONSTRAINT wine_country_fk FOREIGN KEY(country_id) REFERENCES country(country_id),
